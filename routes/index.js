ab9722f9c9c313c233f1351d103275a006a3bfbc
Revanth Kumar <revanth.kumar@colortokens.com>
2021-07-26 22:25:30 +0530

    login api created

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { createUser } = require('./signUp');
+const { createUser, login } = require('./signUp');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,5 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/signUp', createUser)
+router.post('/signUp', createUser);
+router.post('/login', login);
 module.exports = router;
